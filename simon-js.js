7a3cf366376dec5e46b7a04b13b8f8908875614d
Jake J <jakejohnson818@gmail.com>
2018-04-12 16:56:34 -0500

    Mostly done, need to fix bug where no matter what you fail

--- simon-js.js
+++ simon-js.js
@@ -1,78 +1,122 @@
 var game = {
     count: 0,
-    possibilities: ['#green','#blue', '#red', '#dark'],
+    possibilities: ['#green','#blue', '#red', '#yellow'],
     currentGame: [],
     player: [],
     strict: false
 };
 
+$(".start").click(function() {
+    newGame()
+});
+
 function newGame() {
     start();
+}
+
+function reset() {
+    game.player = []
 }
 
 function start() {
     game.currentGame = [];
     game.player = [];
     game.count = 0;
-    addCount();
     generateMove();
 }
-
-function addCount() {
-    game.count++;
-
-}
-
-$(".start").click(function() {
-    newGame()
-});
 
 function generateMove(){
     game.currentGame.push(game.possibilities[(Math.floor(Math.random()*4))]);
     showMoves();
+    console.log(game.currentGame);
 }
 
 function showMoves() {
+    var i = 0;
+    var moves = setInterval(function(){
+        playGame(game.currentGame[i]);
+        i++;
+        if (i >= game.currentGame.length) {
+            clearInterval(moves);
+        }
+    }, 600);
 
+    reset();
+}
+
+function playGame(color) {
+    $(color).addClass('light');
+    setTimeout(function(){
+        $(color).removeClass('light');
+    }, 300);
 }
 
 
 
+function addToPlayer(id) {
+    var color = "#"+id;
+    console.log(color);
+    game.player.push(color);
+    playerTurn();
+}
 
+function playerTurn(x) {
+    if (game.player.valueOf()!== game.currentGame.valueOf()) {
+            alert('Game Over');
+            newGame();
+    } else {
+            alert('Next round!');
+            nextLevel();
+            }
+}
+
+function nextLevel() {
+    addCount();
+}
+
+function addCount() {
+    game.count++;
+    generateMove();
+}
 
 $("#green").click(function(event) {
-    $(this).addClass("lightGreen");
+    $(this).addClass("light");
     setTimeout(function () {
-        $("#green").removeClass("lightGreen");
+        $("#green").removeClass("light");
     }, 300);
-    game.player.push(this.id);
+    game.player.push("#" + this.id);
     console.log(game.player);
 });
 
-
 $("#red").click(function(event) {
-    $(this).addClass("lightRed");
+    $(this).addClass("light");
     setTimeout(function () {
-        $("#red").removeClass("lightRed");
+        $("#red").removeClass("light");
     }, 300);
-    game.player.push(this.id);
+    game.player.push("#" + this.id);
     console.log(game.player);
 });
 
 $("#blue").click(function(event) {
-    $(this).addClass("lightBlue");
+    $(this).addClass("light");
     setTimeout(function () {
-        $("#blue").removeClass("lightBlue");
+        $("#blue").removeClass("light");
     }, 300);
-    game.player.push(this.id);
+    game.player.push("#" + this.id);
     console.log(game.player);
 });
 
 $("#yellow").click(function(event) {
-    $(this).addClass("lightYellow");
+    $(this).addClass("light");
     setTimeout(function () {
-        $("#yellow").removeClass("lightYellow");
+        $("#yellow").removeClass("light");
     }, 300);
-    game.player.push(this.id);
+    game.player.push("#" + this.id);
     console.log(game.player);
+});
+
+$(document).keypress(function(e) {
+    if(e.which == 13) {
+        addToPlayer();
+    }
 });
